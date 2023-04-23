4c605702f13e39eaa4f7892b654dc99b3fb63400
sifue <yoshimura@soichiro.org>
2023-04-23 19:02:35 +0900

    生成のパラメーターのバリエーションを増やした

--- src/pages/output.tsx
+++ src/pages/output.tsx
@@ -3,6 +3,8 @@ import React, { Fragment, useState, useEffect, SyntheticEvent } from 'react'
 import { useRouter } from "next/router";
 import { ThreeDots } from 'react-loader-spinner'
 
+import { PlotParameter, PARAMETERS } from '../components/parameters'
+
 type OutputState = {
     isPlotGenerated: boolean,
     label: string,
@@ -19,33 +21,53 @@ type ApiResponse = {
         what: string,
         how: string,
         why: string
-      },
+    },
     plot: string
+}
+
+function transformIdToName(idParameters: any) {
+    idParameters.novelist = getNameFromId(PARAMETERS.novelist, idParameters.novelist);
+    idParameters.genre = getNameFromId(PARAMETERS.genre, idParameters.genre);
+    idParameters.when = getNameFromId(PARAMETERS.when, idParameters.when);
+    idParameters.where = getNameFromId(PARAMETERS.where, idParameters.where);
+    idParameters.who = getNameFromId(PARAMETERS.who, idParameters.who);
+    idParameters.what = getNameFromId(PARAMETERS.what, idParameters.what);
+    idParameters.how = getNameFromId(PARAMETERS.how, idParameters.how);
+    idParameters.why = getNameFromId(PARAMETERS.why, idParameters.why);
+}
+
+function getNameFromId(parameter: PlotParameter[], id: string) {
+    return parameter.find((p) => p.id === parseInt(id))?.name
 }
 
 export default function Output() {
     const [state, setState] = useState<OutputState>({ isPlotGenerated: false, label: '作成中です、しばらくお待ちください', plot: '' } as OutputState);
     const router = useRouter();
+    const nps = { ...router.query };
+    transformIdToName(nps);
 
     useEffect(() => {
         if (typeof window === "undefined") return; // サーバーサイドレンダリング時には実行しない
-        const fetchData = async () => {
-          try {
-            const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(router.query)
-            };
+        if (!router.query) return; // パラメータがない場合は実行しない
 
-            const response = await fetch('/api/plot.generate', requestOptions);
-            const jsonResponse: ApiResponse = await response.json();
-            setState({ isPlotGenerated: true, label: 'AIによる生成プロット', plot: jsonResponse.plot });
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
+        const fetchData = async () => {
+            try {
+
+                const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(router.query)
+                };
+
+                const response = await fetch('/api/plot.generate', requestOptions);
+                const jsonResponse: ApiResponse = await response.json();
+                setState({ isPlotGenerated: true, label: 'AIによる生成プロット', plot: jsonResponse.plot });
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         };
         fetchData();
-      }, []);
+    }, []);
 
     return (
         <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
@@ -81,7 +103,45 @@ export default function Output() {
 
                     <p className="text-base font-semibold leading-7 text-indigo-600"><Link href="/">戻る</Link></p>
                     <h1 className="mt-5 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">{state.label}</h1>
-                    { !state.isPlotGenerated ? (<div className='w-full h-full flex h-40 py-10 items-center justify-center'><ThreeDots
+
+                    <div className="mx-auto max-w-4xl mt-6 border-t border-gray-100">
+                        <dl className="divide-y divide-gray-100">
+                            <div className="px-20 py-3 sm:grid sm:grid-cols-3 sm:gap-0 sm:px-0">
+                                <dt className="text-sm font-medium leading-4 text-gray-900">作風の元となる小説家</dt>
+                                <dd className="mt-1 text-sm leading-4 text-gray-700 sm:col-span-1 sm:mt-0">{nps.novelist}</dd>
+                            </div>
+                            <div className="px-20 py-3 sm:grid sm:grid-cols-3 sm:gap-0 sm:px-0">
+                                <dt className="text-sm font-medium leading-4 text-gray-900">ジャンル</dt>
+                                <dd className="mt-1 text-sm leading-4 text-gray-700 sm:col-span-2 sm:mt-0">{nps.genre}</dd>
+                            </div>
+                            <div className="px-20 py-3 sm:grid sm:grid-cols-3 sm:gap-0 sm:px-0">
+                                <dt className="text-sm font-medium leading-4 text-gray-900">いつ</dt>
+                                <dd className="mt-1 text-sm leading-4 text-gray-700 sm:col-span-2 sm:mt-0">{nps.when}</dd>
+                            </div>
+                            <div className="px-20 py-3 sm:grid sm:grid-cols-3 sm:gap-0 sm:px-0">
+                                <dt className="text-sm font-medium leading-4 text-gray-900">どこ</dt>
+                                <dd className="mt-1 text-sm leading-4 text-gray-700 sm:col-span-2 sm:mt-0">{nps.where}</dd>
+                            </div>
+                            <div className="px-20 py-3 sm:grid sm:grid-cols-3 sm:gap-0 sm:px-0">
+                                <dt className="text-sm font-medium leading-4 text-gray-900">誰が</dt>
+                                <dd className="mt-1 text-sm leading-4 text-gray-700 sm:col-span-2 sm:mt-0">{nps.who}</dd>
+                            </div>
+                            <div className="px-20 py-3 sm:grid sm:grid-cols-3 sm:gap-0 sm:px-0">
+                                <dt className="text-sm font-medium leading-4 text-gray-900">何を</dt>
+                                <dd className="mt-1 text-sm leading-4 text-gray-700 sm:col-span-2 sm:mt-0">{nps.what}</dd>
+                            </div>
+                            <div className="px-20 py-3 sm:grid sm:grid-cols-3 sm:gap-0 sm:px-0">
+                                <dt className="text-sm font-medium leading-4 text-gray-900">どのように</dt>
+                                <dd className="mt-1 text-sm leading-4 text-gray-700 sm:col-span-2 sm:mt-0">{nps.how}</dd>
+                            </div>
+                            <div className="px-20 py-3 sm:grid sm:grid-cols-3 sm:gap-0 sm:px-0">
+                                <dt className="text-sm font-medium leading-4 text-gray-900">なぜ</dt>
+                                <dd className="mt-1 text-sm leading-4 text-gray-700 sm:col-span-2 sm:mt-0">{nps.why}</dd>
+                            </div>
+                        </dl>
+                    </div>
+
+                    {!state.isPlotGenerated ? (<div className='w-full h-full flex h-40 py-10 items-center justify-center'><ThreeDots
                         height="80"
                         width="80"
                         radius="9"
@@ -90,8 +150,8 @@ export default function Output() {
                         wrapperStyle={{}}
                         visible={true}
                     /></div>) : (<></>)}
-                    
-                    <p className="mt-6 text-xl leading-10 text-gray-700 py-4 break-words">
+
+                    <p className="mt-6 text-xl leading-10 text-gray-700 py-4 whitespace-break-spaces">
                         {state.plot}
                     </p>
                     <p className="text-base font-semibold leading-7 text-indigo-600"><Link href="/">戻る</Link></p>
