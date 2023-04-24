4a044b676f6cc5c82be79522c258d3f92bb5f259
sifue <yoshimura@soichiro.org>
2023-04-25 03:44:37 +0900

    langをjaに変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, Head, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head />
       <body>
         <Main />
