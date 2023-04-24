13f58c5895524aa4c22f2d6d29a69f350d0e0865
sifue <yoshimura@soichiro.org>
2023-04-25 04:05:02 +0900

    next-ssoを利用してOGPに対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
 import '@/styles/globals.css'
+import { NextSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextSeo
+        title="'AIプロットメーカー"
+        description="ChatGPTを利用して物語のプロットをお手軽作成"
+        canonical="https://ai-plot-maker.vercel.app/"
+        openGraph={{
+          url: 'https://ai-plot-maker.vercel.app/',
+          title: 'AIプロットメーカー',
+          description: 'OChatGPTを利用して物語のプロットをお手軽作成',
+          images: [
+            {
+              url: 'https://ai-plot-maker.vercel.app/vercel.svg',
+              width: 1024,
+              height: 235,
+              alt: 'AIプロットメーカー',
+              type: 'image/svg+xml',
+            }
+          ],
+          siteName: 'AIプロットメーカー',
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
