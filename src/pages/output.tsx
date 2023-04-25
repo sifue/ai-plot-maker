6aab0d77cae50ad2f58525c0dd3d7cb7061fa15e
Soichiro Yoshimura <yoshimura@soichiro.org>
2023-04-25 12:48:07 +0900

    useEffectの依存配列にasPathも追加

--- src/pages/output.tsx
+++ src/pages/output.tsx
@@ -66,7 +66,7 @@ export default function Output() {
             }
         };
         fetchData();
-    }, [router.query]);
+    }, [router.query, router.asPath]);
 
     return (
         <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
